af3d38ff01a6fa7bf44bab90bb04fc61975b552d
LEDUNOIS Simon <simon.ledunois@gmail.com>
2022-02-23 21:18:03 +0000

    feat: load config from yaml file using viper #7

--- pkg/cmd/root/root.go
+++ pkg/cmd/root/root.go
@@ -2,17 +2,53 @@ package root
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/SLedunois/b3lb/pkg/config"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+const defaultConfigFileName = ".b3lb.yaml"
+
+var defaultConfigPath = fmt.Sprintf("$HOME/%s", defaultConfigFileName)
+var configPath string
+var Config *config.AdminConfig
+
 func NewCmdRoot() *cobra.Command {
-	return &cobra.Command{
+	cobra.OnInitialize(initConfig)
+	rootCmd := &cobra.Command{
 		Use:   "b3lb-admin <command> [flags]",
 		Short: "B3LB admin cli",
 		Long:  `Manage your B3LB cluster from the command line`,
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("In b3lb-admin command")
-		},
+		Run:   func(cmd *cobra.Command, args []string) {},
 	}
+
+	rootCmd.PersistentFlags().StringVar(&configPath, "config", "c", fmt.Sprintf("config file (default is %s)", defaultConfigPath))
+	viper.BindPFlag("config", rootCmd.PersistentFlags().Lookup("config"))
+	viper.SetDefault("config", "$HOME/.b3lb.yaml")
+
+	return rootCmd
+}
+
+func initConfig() {
+	path := viper.GetString("config")
+	if path == defaultConfigPath {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		path = filepath.Join(homeDir, defaultConfigFileName)
+	}
+
+	c, err := config.Load(path)
+	if err != nil {
+		fmt.Println(fmt.Errorf("unable to load configuration: %s", err.Error()))
+		os.Exit(1)
+	}
+
+	Config = &c.Admin
 }
